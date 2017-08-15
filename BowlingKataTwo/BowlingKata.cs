22c6c137881cc4dc1c526cf6fdfa19a30622faa0
Christina <jainatina@gmail.com>
2017-08-14 20:30:34 -0400

    it works

--- BowlingKataTwo/BowlingKata.cs
+++ BowlingKataTwo/BowlingKata.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace BowlingKataTwo
 {
-    class BowlingKata
+    public class BowlingKata
     {
+
+
+
+        private int[] rolls;
+
+        private int count;
+
+        public BowlingKata()
+        {
+            rolls = new int[21];
+        }
+
+        public void Roll(int pins)
+        {
+
+            rolls[count] = pins;
+            count++;
+
+
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int currentroll = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (rolls[currentroll] == 10)
+                {
+                    score = score + rolls[currentroll] + rolls[currentroll + 1] + rolls[currentroll + 2];
+                    currentroll = currentroll + 1;
+                }
+                else if (rolls[currentroll] + rolls[currentroll + 1] == 10)
+                {
+
+                    score = score + rolls[currentroll] + rolls[currentroll + 1] + rolls[currentroll + 2];
+                    currentroll = currentroll + 2;
+
+                }
+                else
+                {
+                    score = score + rolls[currentroll] + rolls[currentroll + 1];
+                    currentroll = currentroll + 2;
+                }
+                
+            }
+
+            return score;
+        }
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BowlingKataTwo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject1
@@ -6,9 +7,67 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+        private BowlingKata game;
+
+
+        [TestInitialize]
+        public void SetUp()
         {
+
+           game = new BowlingKata();
+
+        }
+
+
+        [TestMethod]
+        public void AllZeros()
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+
+        [TestMethod]
+        public void AllOnes()
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+
+            Assert.AreEqual(20, game.GetScore());
+        }
+
+
+        [TestMethod]
+        public void Spares()
+        {
+
+            game.Roll(4);
+            game.Roll(6);
+            game.Roll(2);
+
+            Assert.AreEqual(14, game.GetScore());
+
+        }
+
+        [TestMethod]
+        public void Strike()
+        {
+
+            game.Roll(10);
+            game.Roll(6);
+            game.Roll(2);
+
+            Assert.AreEqual(26, game.GetScore());
+
         }
     }
 }
